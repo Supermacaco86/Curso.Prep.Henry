980718165b4f55c60025e5f1f5e32ce9cd2d005b
unknown <martinfigueroa2103@hotmail.com>
2022-02-17 18:30:28 -0300

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola Mundo";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "86";
+const nuevoNum = 86;
 
 // Crea una variable booleana:
 const nuevoBool = true;
