7395153db74017ee9bcb0a08dfbe1c8c1500afff
unknown <martinfigueroa2103@hotmail.com>
2022-02-18 14:02:37 -0300

    sexto commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -102,9 +102,9 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num / 2 === 0){
+  if (num / 2 === 0)
     return true;
-  }
+  else 
   return false;
 }
 
